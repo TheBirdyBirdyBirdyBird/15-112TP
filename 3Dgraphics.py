7065148ebc1166a7e46982c8a30462769866e4cb
TheBirdyBirdyBirdyBird <birdybirdybirdybird8@gmail.com>
2025-07-29 14:31:50 -0400

    Update 3Dgraphics.py

--- 3Dgraphics.py
+++ 3Dgraphics.py
@@ -1,173 +1,167 @@
 from cmu_graphics import *
-import cv2
-import math
+import math, time
 import numpy as np
-import threading
-import multiprocess
-import multiprocessing
-import dill
+import numba as nb
 
 global π
-π = 3.14159#265358979
+π = 3.14159
 
-# Möller-Trumbore algorithm to determine intersection with triangles
-def intersection(dir, pos, triangle):
-    # if triangle is parallel to ray, it does not intersect
-    epsilon = 0.001
-    if np.dot(triangle.normal, dir) == 0:
-        return 0
-    # [-dirVec, (p2-p1), (p3-p1)] * [t, u, v] = origin - p1
-    # use Cramer's rule to calculate t, u, v
-    p1, p2, p3 = triangle.p1, triangle.p2, triangle.p3
-    detA = np.linalg.det([-dir, p2-p1, p3-p1])
-    dist = np.linalg.det([pos-p1, p2-p1, p3-p1])/detA
-    u = np.linalg.det([-dir, pos-p1, p3-p1])/detA
-    v = np.linalg.det([-dir, p2-p1, pos-p1])/detA
-    # dist < 0 means triangle is behind ray. if u, v, w=1-u-v are <0, there is no intersection
+'''
+clockwise
+       y
+  #####|#####
+  ####/^\####
+-x-----+-----x
+  #####|#####
+  #####|#####
+      -y
+'''
+
+def types(data):
+    for thing in data:
+        print('type: ', nb.typeof(thing), thing)
+
+def Triangle(vertices, color):
+    p1 = np.array(vertices[0])
+    p2 = np.array(vertices[1])
+    p3 = np.array(vertices[2])
+    return np.array([p1, p2, p3, np.cross((p2-p1), (p3-p1)), np.array(color)])
+
+def Sphere(origin):
+    pass
+
+# using numba is 6% faster
+@nb.jit(nb.f8[:,:](nb.i4, nb.i4))
+def numba_zeros(x, y):
+    return np.zeros((x, y))
+
+# converts cols into matrix, also tells numba the correct types
+@nb.jit(nb.f8[:,:](nb.f8[:], nb.f8[:], nb.f8[:]))
+def make3x3NpMatrix(a1, a2, a3):
+    matrix = numba_zeros(3, 3)
+    matrix[0] = a1
+    matrix[1] = a2
+    matrix[2] = a3
+    return matrix.transpose()
+
+def onAppStart(app):
+    app.background = 'black'
+    app.setMaxShapeCount(100000)
+    app.res = [100, 100]
+    app.fov = [π/2, π/2]
+    app.dir = [0, 0]
+    app.pos = np.array([0.0, 0.0, 0.0])
+    app.pixelSize = [app.width/app.res[0], app.height/app.res[1]]
+    app.angRes = [app.fov[0]/app.res[0], app.fov[1]/app.res[1]]
+    tri = Triangle([[-0.1, 1.0, -0.1], [0.1, 1.0, 0.0], [0.0, 1.0, 0.1]], [255.0, 255.0, 255.0])
+    floor = Triangle([[-10.0, 10.0, -1.0], [10.0, 10.0, -1.0], [0.0, -10.0, -1.0]], [122.0, 122.0, 122.0])
+    app.shapes = [np.array([tri, floor]), np.array([tri])]
+    app.cursor = [0, 0]
+    app.t = time.time()
+
+def redrawAll(app):
+    pixels = []
+    Ix, Iy, Iz = app.pos[0], app.pos[1], app.pos[2]
+    triangles = app.shapes[0]
+    spheres = app.shapes[1]
+    v = app.dir[1] - app.fov[1]/2
+    rects = 0
+    for z in range(app.res[1]):
+        t = app.dir[0] - app.fov[0]/2
+        for x in range(app.res[0]):
+            dx, dy, dz = np.sin(t)*np.cos(v), np.cos(t)*np.cos(v), np.sin(v)
+            color = getColor(Ix, Iy, Iz, dx, dy, dz, triangles, spheres)
+            if color.all() != 0.0:
+                rects += 1
+                drawRect(x * app.pixelSize[0], app.height - z * app.pixelSize[1], app.pixelSize[0], app.pixelSize[1], fill=rgb(*color), align='bottom-left')
+            t += app.angRes[0]
+        v += app.angRes[1]
+    print(rects)
+
+def onStep(app):
+    print(time.time()-app.t)
+    app.t=time.time()
+
+def onMousePress(app, mouseX, mouseY):
+    app.cursor = [mouseX, mouseY]
+
+def onMouseDrag(app, mouseX, mouseY):
+    dx = mouseX - app.cursor[0]
+    dz = mouseY - app.cursor[1]
+    app.cursor = [mouseX, mouseY]
+    app.dir[0] += dx * 0.01
+    app.dir[1] -= dz * 0.01
+
+def onKeyPress(app, key, modifiers):
+    if 'shift' in modifiers:
+        app.pos[2] -= 0.1
+    elif key == 'w' or key == 'up':
+        dx = math.sin(app.dir[0]) * 0.1
+        dy = math.cos(app.dir[0]) * 0.1
+        app.pos[0] += dx
+        app.pos[1] += dy
+    elif key == 'a' or key == 'left':
+        dy = math.sin(app.dir[0]) * 0.1
+        dx = math.cos(app.dir[0]) * 0.1
+        app.pos[0] -= dx
+        app.pos[1] += dy
+    elif key == 's' or key == 'down':
+        dx = math.sin(app.dir[0]) * 0.1
+        dy = math.cos(app.dir[0]) * 0.1
+        app.pos[0] -= dx
+        app.pos[1] -= dy
+    elif key == 'd' or key == 'right':
+        dy = math.sin(app.dir[0]) * 0.1
+        dx = math.cos(app.dir[0]) * 0.1
+        app.pos[0] += dx
+        app.pos[1] -= dy
+    elif key == 'space':
+        app.pos[2] += 0.1
+        
+
+def main():
+    runApp(width=500, height=500)
+
+@nb.jit(nb.f8(nb.f8[:], nb.f8[:], nb.f8[:,:]))
+def intersectTriangle(dir, pos, triangle):
+    if np.dot(triangle[3], dir) == 0:
+        return False
+    barycentric = make3x3NpMatrix(-dir, (triangle[1]-triangle[0]), (triangle[2]-triangle[0]))
+    dist, u, v = np.linalg.solve(barycentric, pos-triangle[0])
     if (dist > 0 and u > 0 and v > 0 and 1-u-v > 0):
         return dist
     else:
-        return 0
+        return False
+
+'''#@nb.jit(nb.f8[:](nb.f8[:], nb.f8[:], nb.f8[:,:,:], nb.f8[:,:,:]))
+def getColor(dir, pos, triangles, spheres):
+    #background = [0, 0, 0] #rgb(abs(dx)*255, abs(dy)*255, abs(dz)*255)
+    minDist = None
+    color = np.array([0.0, 0.0, 0.0])
+    for triangle in triangles:
+        intersect = intersectTriangle(dir, pos, triangle)
+        if intersect:
+            if minDist == None or intersect < minDist:
+                minDist = intersect
+                color = triangle[4]
+    return np.array([0.0, 0.0, 0.0])'''
+
+
+#@nb.jit(nb.f8[:](nb.f8, nb.f8, nb.f8, nb.f8, nb.f8, nb.f8, nb.f8[:,:,:], nb.f8[:,:,:]))
+def getColor(x, y, z, dx, dy, dz, triangles, spheres):
+    #background = [0, 0, 0] #rgb(abs(dx)*255, abs(dy)*255, abs(dz)*255)
+    minDist = None
+    color = np.array([0.0, 0.0, 0.0])
+    dir = np.array([dx, dy, dz])
+    pos = np.array([x, y, z])
+    for triangle in triangles:
+        intersect = intersectTriangle(dir, pos, triangle)
+        if intersect:
+            if minDist == None or intersect < minDist:
+                minDist = intersect
+                color = triangle[4]
+    return color
     
-def radians(deg):
-    return deg * π / 180
 
-def degrees(rad):
-    return rad * 180 / π
-
-def fRange(start, stop, step):
-    while start < stop:
-        yield start
-        start += step
-
-def constrain(x, low, high):
-    return max(min(x, high), low)
-
-class Renderer():
-    def __init__(self, app):
-        self.app = app
-        self.triangles = [Triangle([-0.1, 1, -0.1], [0.1, 1, 0], [0, 1, 0.1])]
-        self.pos = np.array([0, 0, 0]) # x, y, z, meters
-        self.dir = [0, 0] # only hor + vert, degrees
-        self.width = 90 # degrees
-        self.height = 90 # degrees
-        self.rays = [100, 100]
-        self.angRes = [self.width/self.rays[0], self.height/self.rays[1]]
-        self.gSrcs = []
-        self.lSrcs = []
-        self.lumRef = 1
-        self.camRad = 0.01 # physical radius of camera in m. Only used for brightness calculations
-        self.pixelDims = [app.width/self.rays[0], app.height/self.rays[1]]
-
-    def render(self, batch):
-        view = []
-        start = self.rays[1]/8*batch
-        stop = start + self.rays[1]/8
-        for i in range(math.floor(start), math.floor(stop)):
-            view.append([])
-            y = i * self.angRes[1] - self.height/2
-            for j in range(self.rays[0]):
-                x = j * self.angRes[0] - self.width/2
-                view[-1].append(self.castRay(x, y))
-        return view
-    
-    def renderSquare(self, x, y, p1, p2, p3, p4, app):
-        #print(p1, p2, p3, p4)
-        # v1: render average of points, assume light source at camera, 100% reflectivity
-        # reflect is proportion of total light scattered by the plane at p1
-        scatter = (radians(self.angRes[0]/2) * radians(self.angRes[1])/2) / (4 * π)
-        absorb = 0 # absorb is proportion of scattered light absorbed by camera
-        for dist in [p1, p2, p3, p4]:
-            if dist:
-                absorb += scatter * math.sin(math.atan(self.camRad / dist) / 2) ** 2
-        if absorb == 0:
-            return 0
-        appLum = math.log(absorb * self.lumRef, 1.1) + 370 # logarithmic brightness is more versatile
-        color = rgb(constrain(appLum, 0, 255), constrain(appLum, 0, 255), constrain(appLum, 0, 255))
-        #print(appLum)
-        drawRect(self.pixelDims[0]*x, self.pixelDims[1]*y, self.pixelDims[0], self.pixelDims[1], 
-                 fill=color, align='bottom-right')
-        return appLum
-        
-    def castRay(self, x, y):
-        gX = math.radians(self.dir[0] + x)
-        gY = math.radians(self.dir[1] + y) 
-        # [math.sin(gX), math.cos(gX)] is length 1. Multiplying math.cos(gY) ensures length of 1 after 
-        # addition of z-term
-        dirVec = np.array([math.sin(gX) * math.cos(gY), math.cos(gX) * math.cos(gY), math.sin(gY)])
-        #if (x, y) == (0, 0):
-            #print(dirVec)
-        minDist = 0 #intersection(dirVec, self.pos, self.triangles[0])
-        if len(self.triangles) > 0:
-            minDist = intersection(dirVec, self.pos, self.triangles[0])
-        for triangle in self.triangles[1:]:
-            dist = intersection(dirVec, self.pos, triangle)
-            if dist and dist < minDist: # if the intersection passes and is closest yet found
-                minDist = dist # update closest intersection
-        return minDist
-
-class Shape():
-    def __init__(self, faces):
-        self.faces = faces        
-
-class Triangle():
-    def __init__(self, p1, p2, p3):
-        self.p1 = np.array(p1)
-        self.p2 = np.array(p2)
-        self.p3 = np.array(p3)
-        self.normal = np.cross((self.p2-self.p1), (self.p3-self.p1))
-        self.a=float(self.normal[0])
-        self.b=float(self.normal[1])
-        self.c=float(self.normal[2])
-    def isParallel(self, dir, epsilon):
-        if abs(dir[0] * self.a + dir[1] * self.b + dir[2] * self.c) < epsilon:
-            return True
-        else:
-            return False
-
-
-class Vertex():
-    def __init__(self, pos):
-        self.pos = np.array(pos)
-
-def onAppStart(app):
-    app.r = Renderer(app)
-    app.setMaxShapeCount(100000)
-
-def redrawAll(app):
-    view = []
-    for i in range(8):
-        for row in app.r.render(i):
-            view.append(row)
-    
-    #p = multiprocessing.Pool(processes=8)
-    '''processes = []
-    for i in range(8):
-        #processes.append(threading.Thread(target=app.r.render, args=(app, i, views)))
-        processes.append(multiprocess.Process(target=app.r.render, args=(app, i, views)))
-    for process in processes:
-        process.start()
-    for process in processes:
-        process.join()'''
-    
-    #for row in view: print(row)
-    for j in range(len(view)):
-        for i in range(len(view[0])):
-            if i >= 1 and j >= 1:
-                app.r.renderSquare(j, i, view[j-1][i-1], view[j-1][i], view[j][i-1], view[j][i], app)
-            
-    '''threads = [threading.Thread(target=app.r.render, args=(app, i)) for i in range(8)]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()'''
-    print('done')
-    #app.r.render(app, 0)
-
-def onStep(app):
-    pass
-
-def main():
-    runApp(width=1000, height=1000)
 
 main()
