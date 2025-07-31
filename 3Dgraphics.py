132f5c0ef9e333cea70eea11689f0ed7d39299a3
TheBirdyBirdyBirdyBird <birdybirdybirdybird8@gmail.com>
2025-07-31 14:26:07 -0400

    Update 3Dgraphics.py

--- 3Dgraphics.py
+++ 3Dgraphics.py
@@ -3,39 +3,15 @@ import math, time
 import numpy as np
 import numba as nb
 from PIL import Image
+import colorsys
 
-global π
-π = 3.14159
+# apparently this works
+# global π
+# π = 3.14159
 
-'''
-clockwise
-       y
-  #####|#####
-  ####/^\####
--x-----+-----x
-  #####|#####
-  #####|#####
-      -y
-'''
-
-def types(data):
-    for thing in data:
-        print('type: ', nb.typeof(thing), thing)
-
-def Triangle(vertices, color):
-    p1 = np.array(vertices[0])
-    p2 = np.array(vertices[1])
-    p3 = np.array(vertices[2])
-    normal = np.cross((p2-p1), (p3-p1))
-    normal /= np.linalg.norm(normal)
-    return np.array([p1, p2, p3, normal, np.array(color)])
-    
-
-def Sphere(origin, radius, color):
-    return np.array(origin + [radius] + color)
-
-def Light(pos):
-    return np.array(pos)
+# bound x between low and high
+def constrain(x, low, high):
+    return max(min(x, high), low)
 
 # using numba is 6% faster
 @nb.jit(nb.f8[:,:](nb.i4, nb.i4))
@@ -51,51 +27,69 @@ def make3x3NpMatrix(a1, a2, a3):
     matrix[2] = a3
     return matrix.transpose()
 
+class Triangle():
+    def __init__(self, vertices, color):
+        self.p1 = np.array(vertices[0])
+        self.p2 = np.array(vertices[1])
+        self.p3 = np.array(vertices[2])
+        self.normal = np.cross((self.p2-self.p1), (self.p3-self.p1))
+        self.normal /= np.linalg.norm(self.normal)
+        self.color = color
+
+    def serialize(self): # converts to a list for easy usage with numba
+        return np.array([self.p1, self.p2, self.p3, self.normal, np.array(self.color)])
+    
+class Sphere():
+    def __init__(self, origin, radius, color):
+        self.origin = origin
+        self.radius = radius
+        self.color = color
+
+    def serialize(self):
+        return np.array(self.origin + [self.radius] + self.color)
+
+class Light():
+    def __init__(self, origin, intensity, color):
+        self.origin = origin
+        self.intensity = intensity
+        self.color = color
+
+    def serialize(self):
+        return np.array(self.origin + [self.intensity] + self.color)
+
 def onAppStart(app):
     app.background = 'black'
-    app.setMaxShapeCount(100000)
-    app.res = [75, 75]
-    app.fov = [π/2, π/2]
+    app.res = [75, 75] # resolution
+    app.fov = [math.pi/2, math.pi/2] # field of view
     app.dir = [0, 0]
-    app.pos = np.array([0.0, 0.0, 0.0])
+    app.pos = np.array([11.0, -15.0, 11.0])
     app.pixelSize = [app.width/app.res[0], app.height/app.res[1]]
-    app.angRes = [app.fov[0]/app.res[0], app.fov[1]/app.res[1]]
+    app.angRes = [app.fov[0]/app.res[0], app.fov[1]/app.res[1]] # angular resolution
+    # some showcase objects
     tri = Triangle([[-0.1, 1.0, 1.9], [0.1, 1.0, 2.0], [0.0, 1.0, 2.1]], [255.0, 255.0, 255.0])
     tri2 = Triangle([[10.0, 20.0, 5.0], [20.0, 10.0, 5.0], [12.5, 12.5, 20.0]], [255.0, 255.0, 255.0])
     floor = Triangle([[-10.0, 10.0, -1.0], [10.0, 10.0, -1.0], [0.0, -10.0, -1.0]], [122.0, 122.0, 122.0])
-    source = Light([10, 10, 10])
-    sph1 = Sphere([0.0, 5.0, 2.0], 1.0, [255.0, 255.0, 255.0])
+    source = Light([10, 10, 10], 1, [247, 245, 207])
+    sph1 = Sphere([0.0, 5.0, 2.0], 1.0, [255.0, 122.0, 0.0])
     app.shapes = [np.array([tri, floor, tri2]), np.array([sph1])]
     app.lights = [source]
-    app.cursor = [0, 0]
-    app.t = time.time()
 
-def redrawAll(app):
-    pixels = []
-    Ix, Iy, Iz = app.pos[0], app.pos[1], app.pos[2]
-    triangles = app.shapes[0]
-    spheres = app.shapes[1]
-    v = app.dir[1] - app.fov[1]/2
-    for z in range(app.res[1]):
-        t = app.dir[0] - app.fov[0]/2
-        for x in range(app.res[0]):
-            dx, dy, dz = np.sin(t)*np.cos(v), np.cos(t)*np.cos(v), np.sin(v)
-            pixels.append(tuple(getColor(Ix, Iy, Iz, dx, dy, dz, triangles, spheres, app.lights).astype(int)))
-            t += app.angRes[0]
-        v += app.angRes[1]
-    frame = Image.new(mode="RGB", size=app.res)
-    frame.putdata(pixels)
-    frame = frame.transpose(Image.FLIP_TOP_BOTTOM)
-    drawImage(CMUImage(frame), 0, 0, width = app.width, height = app.height)
+    app.cursor = [0, 0]
+    app.t = time.time() # track delay between frames
+    app.lightSpeed = 300000000
+    app.motion = [0, 0, 0]
+    app.enableRelativity = True
 
 def onStep(app):
-    print(time.time()-app.t)
+    print(time.time()-app.t) # delay between frames
     app.t=time.time()
+    print(app.pos, app.dir)
 
 def onMousePress(app, mouseX, mouseY):
     app.cursor = [mouseX, mouseY]
 
 def onMouseDrag(app, mouseX, mouseY):
+    # check movement of cursor to adjust camera direction
     dx = mouseX - app.cursor[0]
     dz = mouseY - app.cursor[1]
     app.cursor = [mouseX, mouseY]
@@ -103,42 +97,164 @@ def onMouseDrag(app, mouseX, mouseY):
     app.dir[1] -= dz * 0.01
 
 def onKeyPress(app, key, modifiers):
-    processKey(app, key, modifiers)
+    if key == 'p': # putting this inside onKeyHold caused it to switch randomly unless I flicked the button very fast
+        app.motion = [0,0,0]
+        app.enableRelativity = not app.enableRelativity
 def onKeyHold(app, keys, modifiers):
-    for key in keys:
-        processKey(app, key, modifiers)
+    processKey(app, keys[0], modifiers)
 
 def processKey(app, key, modifiers):
-    if 'shift' in modifiers:
-        app.pos[2] -= 0.1
-    elif key == 'w' or key == 'up':
+    key = key.lower()
+    if key == 'w' or key == 'up':
         dx = math.sin(app.dir[0]) * 0.1
         dy = math.cos(app.dir[0]) * 0.1
         app.pos[0] += dx
         app.pos[1] += dy
+        app.motion = [dx * 10, dy * 10, 0]
     elif key == 'a' or key == 'left':
         dy = math.sin(app.dir[0]) * 0.1
         dx = math.cos(app.dir[0]) * 0.1
         app.pos[0] -= dx
         app.pos[1] += dy
+        app.motion = [-dx * 10, dy * 10, 0]
     elif key == 's' or key == 'down':
         dx = math.sin(app.dir[0]) * 0.1
         dy = math.cos(app.dir[0]) * 0.1
         app.pos[0] -= dx
         app.pos[1] -= dy
+        app.motion = [-dx * 10, -dy * 10, 0]
     elif key == 'd' or key == 'right':
         dy = math.sin(app.dir[0]) * 0.1
         dx = math.cos(app.dir[0]) * 0.1
         app.pos[0] += dx
         app.pos[1] -= dy
-    elif key == 'space':
+        app.motion = [dx * 10, -dy * 10, 0]
+    elif key == 'z' or key == 'space':
         app.pos[2] += 0.1
+        app.motion = [0, 0, 1]
+    elif key == 'x':
+        app.pos[2] -= 0.1
+        app.motion = [0, 0, -1]
+    #adjust speed of light based on log scale. shift for higher sensitivity
+    elif key == 'e':
+        logMetric = math.log(app.lightSpeed) * 20.5
+        if 'shift' in modifiers:
+            if logMetric >= 1:
+                app.lightSpeed = math.e ** ((logMetric - 1)/20.5)
+        elif logMetric >= 10:
+            app.lightSpeed = math.e ** ((logMetric - 10)/20.5)
+    elif key == 'r':
+        logMetric = math.log(app.lightSpeed) * 20.5
+        if 'shift' in modifiers:
+            if logMetric <= 399.2:
+                app.lightSpeed = math.e ** ((logMetric + 1)/20.5)
+        elif logMetric <= 391:
+            app.lightSpeed = math.e ** ((logMetric + 10)/20.5)
 
-def main():
-    runApp(width=500, height=500)
+    #elif key == 'i':
+        #adjustVariable(app)
+
+# intended to let user set certain parameters or add new objects. However, the input() function interacts 
+# strangely with cmu_graphics and it oftentimes causes the program to crash or freeze permanently. 
+def adjustVariable(app):
+    query = int(input("adjust variable: 1=x, 2=y, 3=z, 4=speed of light, 5=3d element, 6=cancel\n"))
+    if query == 6: 
+        print('cancel')
+        return None
+    elif query <= 4 and query >= 1:
+        value = float(input("enter new value\n"))
+        if query == 1:
+            app.pos[0] = value
+        elif query == 2:
+            app.pos[1] = value
+        elif query == 3:
+            app.pos[2] = value
+        elif query == 4:
+            print(app.lightSpeed)
+            app.lightSpeed = value
+            print(app.lightSpeed)
+    elif query == 5:
+        shapeType = int(input("1=Triangle, 2=Sphere, 3=Light\n"))
+        if shapeType == 1:
+            parameters = input("12 space separated numbers: x1 y1 z1 x2 y2 z2 x3 y3 z3 red green blue\n")
+            parameters = [int(parameter) for parameter in parameters.split()]
+            triangle = Triangle(parameters[:9], parameters[9:])
+            app.shapes[0].append(triangle)
+        elif shapeType == 2:
+            parameters = input("7 space separated numbers: x y z radius red green blue\n")
+            parameters = [int(parameter) for parameter in parameters.split()]
+            sphere = Sphere(parameters[:3], parameters[3], parameters[4:])
+            app.shapes[1].append(sphere)
+        elif shapeType == 3:
+            parameters = input("7 space separate numbers: x y z intensity red green blue\n")
+            parameters = [int(parameter) for parameter in parameters.split()]
+            light = Light(parameters[:3], parameters[3], parameters[4:])
+            app.lights.append(light)
+
+def redrawAll(app):
+    renderScreen(app)
+    if app.enableRelativity:
+        drawLightSlider(app)
+
+def renderScreen(app):
+    pixels = []
+    Cx, Cy, Cz = app.pos[0], app.pos[1], app.pos[2]
+    triangles = [triangle.serialize() for triangle in app.shapes[0]]
+    spheres = [sphere.serialize() for sphere in app.shapes[1]]
+    lights = [light.serialize() for light in app.lights]
+    v = app.dir[1] - app.fov[1]/2
+    for z in range(app.res[1]):
+        t = app.dir[0] - app.fov[0]/2
+        for x in range(app.res[0]):
+            dx, dy, dz = np.sin(t)*np.cos(v), np.cos(t)*np.cos(v), np.sin(v)
+            color = getColor( Cx, Cy, Cz, dx, dy, dz, triangles, spheres, lights).astype(int)
+            if app.enableRelativity:
+                color = applyRelativity(color, app.motion, [dx, dy, dz], app.lightSpeed)
+            pixels.append(tuple(color))
+            t += app.angRes[0]
+        v += app.angRes[1]
+    frame = Image.new(mode="RGB", size=app.res)
+    frame.putdata(pixels)
+    frame = frame.transpose(Image.FLIP_TOP_BOTTOM)
+    drawImage(CMUImage(frame), 0, 0, width = app.width, height = app.height)
+
+def applyRelativity(color, motion, rayDir, c):
+    relSpeed = -np.dot(motion, rayDir)
+    wavelengthRatio = ((1 + relSpeed/c) / (1 - relSpeed/c)) ** 0.5 # Doppler factor, Wikipedia
+    color = list(colorsys.rgb_to_hsv(*(color/255))) # HSV colors are easier to adjust to match relativistic effects
+
+    # Doppler effect
+    # convert hue to wavelength linearly, red (0)->700, deep purple (5/6)->380
+    wavelength = (1-color[0]) * 384 + 316 
+    
+    observed = wavelengthRatio * wavelength
+    if observed > 700:
+        color[2] /= observed - 699
+        observed = 700
+    elif observed < 380:
+        color[2] /= 381 - observed
+        observed = 380
+    color[0] = (316 - observed) / 384 + 1
+
+    # Headlight effect
+    color[1] = constrain(color[1] * wavelengthRatio ** 2, 0, 1)
+
+    #reformat data
+    color = (np.array(colorsys.hsv_to_rgb(*color)) * 255).astype(int)
+    color[0] = constrain(color[0], 0, 255)
+    color[1] = constrain(color[1], 0, 255)
+    color[2] = constrain(color[2], 0, 255)
+    return color
+
+def drawLightSlider(app):
+    drawRect(50, 50, 400, 20, fill='white', border='black')
+    sliderPos = math.log(app.lightSpeed) * 20.5 + 50 # uses logarithmic scale
+    drawRect(sliderPos, 45, 30, 30, fill='black', border='white')
+    drawLabel(f'light speed is {rounded(app.lightSpeed)} m/s', 250, 90, fill='blue', size=20)
 
 @nb.jit(nb.f8(nb.f8[:], nb.f8[:], nb.f8[:,:]))
 def intersectTriangle(dir, pos, triangle):
+    # Moller-Trumbore algorithm, did not use code references to help. 
     triangle = np.ascontiguousarray(triangle)
     dir = np.ascontiguousarray(dir)
     if np.dot(triangle[3], dir) == 0:
@@ -149,19 +265,21 @@ def intersectTriangle(dir, pos, triangle):
         return dist
     else:
         return False
-    
+
 @nb.jit(nb.f8(nb.f8[:], nb.f8[:], nb.f8[:]))
 def intersectSphere(dir, pos, sphere):
-    dir = np.ascontiguousarray(dir)
+    # self derived
+    dir = np.ascontiguousarray(dir) # dot product is faster on contigious arrays
     relPos = sphere[:3] - pos
     projection = np.dot(dir, relPos)
     if projection < 0: return False
-    d = (np.linalg.norm(relPos) ** 2 - projection ** 2) ** 0.5
-    if d > sphere[3]: return False
-    dist = projection - (sphere[3] ** 2 - d ** 2) ** 0.5
+    altitude = (np.linalg.norm(relPos) ** 2 - projection ** 2) ** 0.5 # I can't think of a good name
+    if altitude > sphere[3]: return False
+    dist = projection - (sphere[3] ** 2 - altitude ** 2) ** 0.5
     return dist
 
 def findIntersection(dir, pos, triangles, spheres):
+    # loop through all shapes for closest intersection
     minDist = None
     color = np.array([0.0, 0.0, 0.0])
     for triangle in triangles:
@@ -182,26 +300,26 @@ def findIntersection(dir, pos, triangles, spheres):
         return np.array(np.concatenate((np.array([minDist]), color, normal)))
     else: return None
 
-#@nb.jit(nb.f8[:](nb.f8, nb.f8, nb.f8, nb.f8, nb.f8, nb.f8, nb.f8[:,:,:], nb.f8[:,:,:]))
 def getColor(x, y, z, dx, dy, dz, triangles, spheres, lights):
-    #background = [0, 0, 0] #rgb(abs(dx)*255, abs(dy)*255, abs(dz)*255)
-    minDist = None
     color = np.array([0.0, 0.0, 0.0])
     dir = np.array([dx, dy, dz])
     pos = np.array([x, y, z])
-    intersection = findIntersection(dir, pos, triangles, spheres)
+    intersection = findIntersection(dir, pos, triangles, spheres) #first find what the ray hits
     if isinstance(intersection, type(None)):
-        return color
-    rayPos = pos + dir * (intersection[0] - 0.001)
-    illumination = 0
-    for light in lights:
-        rayDir = light - rayPos
+        return color # return default color if it hits nothing
+    rayPos = pos + dir * (intersection[0] - 0.001) # slightly behind where the ray intersects, prepares for lighting
+    illumination = np.array([0.1, 0.1, 0.1]) # start with some subtle ambient lighting
+    for light in lights: # if light has line of sight with intersection location, apply lighting from that light
+        rayDir = light[:3] - rayPos 
         dist = np.linalg.norm(rayDir)
         rayDir /= dist
-        blockers = findIntersection(rayDir, rayPos, triangles, spheres)
+        blockers = findIntersection(rayDir, rayPos, triangles, spheres) # check line of sight
         if isinstance(blockers, type(None)) or blockers[0] > dist:
-            illumination += 100 * abs(np.dot(intersection[4:], rayDir)) * 1/dist ** 2
-    color = intersection[1:4] * illumination + 25
+            illumination += light[4:] * light[3] * abs(np.dot(intersection[4:], rayDir)) * 1/dist ** 2 # apply
+    color = np.multiply(intersection[1:4], illumination)
     return color
+
+def main():
+    runApp(width=500, height=500)
 
 main()
